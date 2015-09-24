e5fa6b4cca0dada24aa8bfde02b7581cd8e031f9
paranoodle <paranoodle@users.noreply.github.com>
2015-09-24 12:43:46 +0200

    server theoretically functional

--- Labo 1/src/PTP_Server.java
+++ Labo 1/src/PTP_Server.java
@@ -6,47 +6,61 @@ public class PTP_Server {
     
     private static final int SERVER_SOCKET = 4446;
     private static final int CLIENT_SOCKET = 4445;
-    private static final int MULTICAST_DELAY = 2000;
+    private static final int MULTICAST_DELAY = 1000;
     
     public static void main(String[] args) throws IOException {
+        
+        boolean end = false;
         
         // handles sync and follow-up messages at a set interval
         Thread multicast = new Thread() {
             public void run() {
+                
                 // initializing variables
                 int id = 0;
                 long time;
                 byte[] buffer;
                 DatagramPacket packet;
                 
-                // group of clients
-                System.out.println("STARTING MULTICAST SERVER...");
-                InetAddress server = InetAddress.getLocalHost();
-                MulticastSocket socket = new MulticastSocket(SERVER_SOCKET);
-                System.out.println("Multicast server address is: " + server + "\n");
+                try {
+                    // multicast set-up
+                    System.out.println("STARTING MULTICAST SERVER...");
+                    InetAddress server = InetAddress.getLocalHost();
+                    MulticastSocket socket = new MulticastSocket(SERVER_SOCKET);
+                    System.out.println("Multicast server address is: " + server + "\n");
                 
-                while(true) {
-                    // SYNC message multicast
-                    buffer = PTP_Shared.makeMessage(PTP_Shared.SYNC, id++); // ID incremented!
-                    packet = new DatagramPacket(buffer, 5, server, CLIENT_SOCKET);
-                    time = System.currentTimeMillis();
-                    socket.send(packet);
-                    System.out.println("SYNC packet sent, size " + (buffer.length) + " bytes");
+                    while(!end) {
+                        // SYNC message multicast
+                        buffer = PTP_Shared.makeMessage(PTP_Shared.SYNC, id++); // ID incremented!
+                        packet = new DatagramPacket(buffer, 5, server, CLIENT_SOCKET);
+                        time = System.currentTimeMillis();
+                        socket.send(packet);
+                        System.out.println("SYNC packet sent");
+                        
+                        // FOLLOW_UP message multicast
+                        buffer = PTP_Shared.makeTimeMessage(PTP_Shared.FOLLOW_UP, id, time);
+                        packet = new DatagramPacket(buffer, 13, server, CLIENT_SOCKET);
+                        socket.send(packet);
+                        System.out.println("FOLLOW_UP packet sent");
+                        
+                        Thread.sleep(MULTICAST_DELAY);
+                    }
                     
-                    // FOLLOW_UP message multicast
-                    buffer = PTP_Shared.makeTimeMessage(PTP_Shared.FOLLOW_UP, id, time);
-                    packet = new DatagramPacket(buffer, 13, server, CLIENT_SOCKET);
-                    socket.send(packet);
-                    System.out.println("FOLLOW_UP packet sent, size " + (buffer.length) + " bytes");
+                    socket.close();
+                    
+                } catch (Exception e) {
+                    System.out.println("ERROR IN MULTICAST THREAD");
+                    return;
                 }
                 
-                socket.close();
+                //socket.close();
             }
-        }
+        };
         
         // handles responding to delay messages
         Thread response = new Thread() {
             public void run() {
+                
                 // initializing variables
                 int id;
                 long time;
@@ -55,34 +69,42 @@ public class PTP_Server {
                 InetAddress client;
                 int port;
                 
-                System.out.println("STARTING RESPONSE THREAD...");
-                DatagramSocket socket = new DatagramSocket(CLIENT_SOCKET);
-                
-                while(true) {
-                    buffer = new byte[PTP_Shared.MESSAGE_SIZE];
-                    packet = new DatagramPacket(buffer, buffer.length);
+                try {
+                    // response set-up
+                    System.out.println("STARTING RESPONSE THREAD...");
+                    DatagramSocket socket = new DatagramSocket(CLIENT_SOCKET);
                     
-                    // DELAY_REQUEST reception
-                    socket.receive(packet);
-                    time = System.currentTimeMillis();
-                    
-                    if (PTP_Shared.getMessageType(packet.getData()) == PTP_Shared.DELAY_REQUEST) {
-                        id = PTP_Shared.getMessageID(packet.getData());
-                        client = packet.getAddress();
-                        port = packet.getPort();
-                        System.out.println("Received delay_request packet #" + id + " from " + client + ":" + port);
+                    while(!end) {
+                        buffer = new byte[PTP_Shared.MESSAGE_SIZE];
+                        packet = new DatagramPacket(buffer, buffer.length);
                         
-                        // DELAY_RESPONSE emission
-                        buffer = PTP_Shared.makeTimeMessage(PTP_Shared.DELAY_RESPONSE, id, time);
-                        packet = new DatagramPacket(buffer, buffer.length, client, port);
-                        socket.send(packet);
-                        System.out.println("Sent delay_response #" + id + " to " + client + ":" + port);
+                        // DELAY_REQUEST reception
+                        socket.receive(packet);
+                        time = System.currentTimeMillis();
+                        
+                        if (PTP_Shared.getMessageType(packet.getData()) == PTP_Shared.DELAY_REQUEST) {
+                            // obtaining packet data
+                            id = PTP_Shared.getMessageID(packet.getData());
+                            client = packet.getAddress();
+                            port = packet.getPort();
+                            System.out.println("Received delay_request packet #" + id + " from " + client + ":" + port);
+                            
+                            // DELAY_RESPONSE emission
+                            buffer = PTP_Shared.makeTimeMessage(PTP_Shared.DELAY_RESPONSE, id, time);
+                            packet = new DatagramPacket(buffer, buffer.length, client, port);
+                            socket.send(packet);
+                            System.out.println("Sent delay_response #" + id + " to " + client + ":" + port);
+                        }
                     }
+                    
+                    socket.close();
+                    
+                } catch (Exception e) {
+                    System.out.println("ERROR IN RESPONSE THREAD");
+                    return;
                 }
-                
-                socket.close();
             }
-        }
+        };
         
         response.start();
         multicast.start();
